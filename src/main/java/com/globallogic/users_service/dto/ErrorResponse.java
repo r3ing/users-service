14537b8942648c1bcc91c85c7a4e9382a165c0bb
Rey <rey@192.168.1.5>
2025-09-07 18:03:09 -0300

    feat: centralize exception handling

---
+++ src/main/java/com/globallogic/users_service/dto/ErrorResponse.java
@@ -0,0 +1,34 @@
+package com.globallogic.users_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class ErrorResponse {
+
+    @Setter
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class ErrorItem {
+        private Instant timestamp;
+        private int codigo;
+        private String detail;
+
+    }
+
+    private List<ErrorItem> error;
+
+    public ErrorResponse(Instant timestamp, int codigo, String detail) {
+        this.error = Collections.singletonList(new ErrorItem(timestamp, codigo, detail));
+    }
+
+}
