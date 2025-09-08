d2e5ad927f452098164235f15c6b61bcec4b788e
Rey <rey@192.168.1.5>
2025-09-08 08:41:41 -0300

    test: add unit test

--- src/main/java/com/globallogic/users_service/dto/PhoneRequest.java
+++ src/main/java/com/globallogic/users_service/dto/PhoneRequest.java
@@ -1,8 +1,12 @@
 package com.globallogic.users_service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PhoneRequest {
     private Long number;
     private Integer citycode;
--- src/main/java/com/globallogic/users_service/dto/SignUpRequest.java
+++ src/main/java/com/globallogic/users_service/dto/SignUpRequest.java
@@ -2,6 +2,7 @@ package com.globallogic.users_service.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class SignUpRequest {
     private String name;
 
